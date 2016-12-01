c70e344a4c1a30d1ff93a62cea8b66fa72165155
Penthious <tleffew1994@gmail.com>
2016-11-30 17:29:08 -0700

    added checks to see if simon and user are the same

--- src/components/SimonColor.jsx
+++ src/components/SimonColor.jsx
@@ -5,17 +5,25 @@ import React, {
 
 class SimonColor extends Component {
     handleOnClick = (color) => {
-        this.props.handleOnClick(color);
-    }
+        this.props.handleColorClick(color);
+    };
+
     render() {
         return (
-            <div className="inner-box" style={{backgroundColor: this.props.color}} onClick={() => this.handleOnClick(this.props.color)}>
-            </div>
+            <div
+                className="inner-box"
+                style={{ backgroundColor: this.props.color }}
+                onClick={() => this.handleOnClick(this.props.color)}
+            />
+
         );
     }
 }
 
-SimonColor.propTypes    = {};
+SimonColor.propTypes    = {
+    color: PropTypes.string,
+    handleColorClick: PropTypes.func,
+};
 SimonColor.defaultProps = {};
 
 export default SimonColor;
--- src/components/SimpleSimon.jsx
+++ src/components/SimpleSimon.jsx
@@ -5,17 +5,63 @@ import SimonColor from './SimonColor';
 import './styles.sass';
 
 class SimpleSimon extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            simonColors: [],
+            userColors: [],
+        };
+    }
+
+    onSuccess = () => {
+        const simon = this.state.simonColors;
+        const user  = this.state.userColors;
+        for (let i = 0; i < user.length; i++) {
+            if (simon.length === user.length - 1) {
+                this.clearUsersColors();
+                this.startSimon();
+            } else if (simon[i] !== user[i]) {
+                console.log('you lose');
+            } else {
+            }
+        }
+    }
+
+    clearUsersColors = () => {
+        this.setState({
+            userColors: [],
+        });
+    }
+
+    startSimon = () => {
+        console.log(this.state.simonColors);
+        this.pickRandom();
+        this.onSuccess();
+    };
+
+    pickRandom = () => {
+        const colors = ['red', 'blue', 'green', 'yellow'];
+        const item   = colors[Math.floor(Math.random() * colors.length)];
+        this.setState({
+            simonColors: this.state.simonColors.concat(item),
+        });
+    };
+
     handleColorClick = (color) => {
-        console.log(color);
+        this.setState({
+            userColors: this.state.userColors.concat(color),
+        });
+        console.log(this.state.userColors);
     };
 
     render() {
         return (
             <div className="box">
-                <SimonColor color="red" handleColorClick={this.handleColorClick}/>
-                <SimonColor color="blue" handleColorClick={this.handleColorClick}/>
-                <SimonColor color="green" handleColorClick={this.handleColorClick}/>
-                <SimonColor color="yellow" handleColorClick={this.handleColorClick}/>
+                <SimonColor color="red" handleColorClick={this.handleColorClick} />
+                <SimonColor color="blue" handleColorClick={this.handleColorClick} />
+                <SimonColor color="green" handleColorClick={this.handleColorClick} />
+                <SimonColor color="yellow" handleColorClick={this.handleColorClick} />
+                <button onClick={this.startSimon}> start</button>
             </div>
         );
     }
